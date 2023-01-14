1446b1c513a2bb2967d03a112efb7a5f4ce7db67
Basanta Rijal <rijalbasanta123@gmail.com>
2023-01-14 13:07:51 +0545

    Implement tests for CRUD functions on the entries and transfer tables

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,77 @@
+package db
+
+import (
+	"backend-go/db/utils"
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, from Account, to Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: from.ID,
+		ToAccountID:   to.ID,
+		Amount:        utils.RandomInt(0, 1000),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	createRandomTransfer(t, fromAccount, toAccount)
+}
+
+func TestGetTransfer(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, fromAccount, toAccount)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfers(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, fromAccount, toAccount)
+	}
+	arg := ListTransfersParams{
+		FromAccountID: fromAccount.ID,
+		ToAccountID:   toAccount.ID,
+		Limit:         3,
+		Offset:        5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 3)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+
+		require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+		require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	}
+}
--- db/utils/random.go
+++ db/utils/random.go
@@ -14,7 +14,7 @@ func init() {
 
 // RandomInt generates a random integer between min and max
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(max-min+1) // 0->max-min
+	return min + rand.Int63n(max-min+1) // min-max
 }
 
 // RandomString generates a random string of length n
