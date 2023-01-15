f29303e5d408aef986f1905c08c6c10a0120d0ac
Basanta Rijal <rijalbasanta123@gmail.com>
2023-01-16 01:11:18 +0545

    Implement RESTful APIs for CRUD operations of Accounts

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"backend-go/api"
+	db "backend-go/db/sqlc"
+	"backend-go/db/utils"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
